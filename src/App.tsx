14c9cdcffe253291dc0bd51047d703845e84a2a5
Diego Giuliani <giuliani.diego@gmail.com>
2021-01-17 18:27:09 -0300

    Add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header } from './components/layout/header';
 
 import './App.css';
 import { Content } from './components/layout/content';
 import { ProjectProvider, SelectedProjectProvider } from './context';
+import classNames from 'classnames';
+interface Props {
+  darkModeDefault: boolean;
+}
 
-function App() {
+function App({ darkModeDefault = false }: Partial<Props>) {
+  const [darkMode, setDarkMode] = useState(darkModeDefault);
+  console.log(darkMode);
   return (
     <ProjectProvider>
       <SelectedProjectProvider>
-        <div className="App">
-          <Header />
+        <main
+          data-testid="application"
+          className={classNames({ App: true, darkmode: darkMode })}
+        >
+          <Header darkMode={darkMode} setDarkMode={setDarkMode} />
           <Content />
-        </div>
+        </main>
       </SelectedProjectProvider>
     </ProjectProvider>
   );
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction, useState } from 'react';
 import { FaPizzaSlice } from 'react-icons/fa';
-export const Header = () => {
+interface Props {
+  darkMode: boolean;
+  setDarkMode: Dispatch<SetStateAction<boolean>>;
+}
+export const Header = ({ darkMode, setDarkMode }: Props) => {
+  const [shouldShowMain, setShouldShowMain] = useState(false);
+  const [showQuickAddTask, setShowQuickAddTask] = useState(false);
   return (
     <header className="header" data-testid="header">
       <nav>
@@ -12,7 +18,12 @@ export const Header = () => {
             <li data-testid="quick-add-task-action" className="settings__add">
               +
             </li>
-            <li data-testid="dark-mode-action" className="settings__darkmode">
+            <li
+              data-testid="dark-mode-action"
+              className="settings__darkmode"
+              onClick={() => setDarkMode(!darkMode)}
+              onKeyDown={(e) => e.key === 'ENTER' && setDarkMode(!darkMode)}
+            >
               <FaPizzaSlice />
             </li>
           </ul>
