3e862cbf5151eb7f89ace3cdb1e7522048439b1c
Diego Giuliani <giuliani.diego@gmail.com>
2021-01-21 19:43:26 -0300

    Fix tests for checkbox

--- src/__tests__/checkbox.spec.js
+++ src/__tests__/checkbox.spec.js
@@ -18,38 +18,36 @@ beforeEach(() => {
 });
 
 describe('<Checkbox />', () => {
-  describe('Success', () => {
-    it('renders the task checkbox', () => {
-      const { queryByTestId } = render(
-        <Checkbox id="1" taskDesc="Finish this tutorial series!" />
-      );
-      expect(queryByTestId('checkbox-action')).toBeTruthy();
-    });
+  it('renders the task checkbox', () => {
+    const { queryByTestId } = render(
+      <Checkbox id="1" taskDesc="Finish this tutorial series!" />
+    );
+    expect(queryByTestId('checkbox-action')).toBeTruthy();
+  });
 
-    it('renders the task checkbox and accepts a onClick', () => {
-      const { queryByTestId } = render(
-        <Checkbox id="1" taskDesc="Finish this tutorial series!" />
-      );
-      expect(queryByTestId('checkbox-action')).toBeTruthy();
-      fireEvent.click(queryByTestId('checkbox-action'));
-      expect(firebase.firestore).toHaveBeenCalled();
-    });
+  it('renders the task checkbox and accepts a onClick', () => {
+    const { queryByTestId } = render(
+      <Checkbox id="1" taskDesc="Finish this tutorial series!" />
+    );
+    expect(queryByTestId('checkbox-action')).toBeTruthy();
+    fireEvent.click(queryByTestId('checkbox-action'));
+    expect(firebase.firestore).toHaveBeenCalled();
+  });
 
-    it('renders the task checkbox and accepts a onKeyDown', () => {
-      const { queryByTestId } = render(
-        <Checkbox id="1" taskDesc="Finish this tutorial series!" />
-      );
-      expect(queryByTestId('checkbox-action')).toBeTruthy();
-      fireEvent.keyDown(queryByTestId('checkbox-action'), {
-        key: 'a',
-        code: 65,
-      });
-      expect(firebase.firestore).toHaveBeenCalledTimes(0);
-      fireEvent.keyDown(queryByTestId('checkbox-action'), {
-        key: 'Enter',
-        code: 13,
-      });
-      expect(firebase.firestore).toHaveBeenCalled();
+  it('renders the task checkbox and accepts a onKeyDown', () => {
+    const { queryByTestId } = render(
+      <Checkbox id="1" taskDesc="Finish this tutorial series!" />
+    );
+    expect(queryByTestId('checkbox-action')).toBeTruthy();
+    fireEvent.keyDown(queryByTestId('checkbox-action'), {
+      key: 'a',
+      code: 65,
     });
+    expect(firebase.firestore).toHaveBeenCalledTimes(0);
+    fireEvent.keyDown(queryByTestId('checkbox-action'), {
+      key: 'Enter',
+      code: 13,
+    });
+    expect(firebase.firestore).toHaveBeenCalled();
   });
 });
